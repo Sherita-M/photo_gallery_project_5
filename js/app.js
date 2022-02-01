592262a2694ff2c6008d0bf5930b7f86b278aace
Sherita Manning <smanningsn@gmail.com>
2022-01-31 19:35:24 -0800

    Updated input and JS scripts in html

--- js/app.js
+++ js/app.js
@@ -1,2 +1,2 @@
-baguetteBox.run('.gallery');
-const search = new Filter('search', 'data-caption');;​
+window.addEventListener('load', function() { baguetteBox.run('.gallery'); });
+const search = new Filter('search', 'data-caption');
